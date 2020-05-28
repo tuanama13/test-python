ede7c97e16f28474cfac1e8d623b02b6c7ae23fc
tuanama13 <tuanama7@gmail.com>
2020-05-29 03:12:50 +0700

    Add Other Python

---
+++ kondisi.py
@@ -0,0 +1,40 @@
+#Kondisi if adalah kondisi yang akan dieksekusi oleh program jika bernilai benar atau TRUE
+
+nilai = 9
+
+#jika kondisi benar/TRUE maka program akan mengeksekusi perintah dibawahnya
+if(nilai > 7):
+    print("Selamat Anda Lulus")
+
+#jika kondisi salah/FALSE maka program tidak akan mengeksekusi perintah dibawahnya
+if(nilai > 10):
+    print("Selamat Anda Lulus")
+
+
+#Kondisi if else adalah jika kondisi bernilai TRUE maka akan dieksekusi pada if, tetapi jika bernilai FALSE maka akan dieksekusi kode pada else
+
+nilai = 8
+#Jika pernyataan pada if bernilai TRUE maka if akan dieksekusi, tetapi jika FALSE kode pada else yang akan dieksekusi.
+if(nilai > 7):
+    print("Selamat Anda Lulus")
+else:
+    print("Maaf Anda Tidak Lulus")
+
+#Contoh penggunaan kondisi elif
+
+hari_ini = "Minggu"
+
+if(hari_ini == "Senin"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Selasa"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Rabu"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Kamis"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Jumat"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Sabtu"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Minggu"):
+    print("Saya akan libur")
---
+++ string.py
@@ -0,0 +1,18 @@
+name = 'John Doe' 
+message = "John Doe belajar bahasa python di Belajarpython"
+print ("name[0]: ", name[0])
+print ("message[1:4]: ", message[1:4])
+
+# update
+message = 'Hello World'
+print ("Updated String :- ", message[:6] + 'Python')
+
+
+kutipantiga = """this is a long string that is made up of
+several lines and non-printable characters such as
+TAB ( \t ) and they will show up that way when displayed.
+NEWLINEs within the string, whether explicitly given like
+this within the brackets [ \n ], or just a NEWLINE within
+the variable assignment will also show up.
+"""
+print (kutipantiga)
